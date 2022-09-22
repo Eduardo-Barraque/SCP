290ce603f8b59212926229814613104ca6fac012
Eduardo-Barraque <eduabarraque@gmail.com>
2022-09-22 14:27:01 -0300

    Presença fully working

---
+++ src/main/java/br/edu/yuniversidadedevassouras/SCP/Controller/PresencaController.java
@@ -0,0 +1,43 @@
+package br.edu.yuniversidadedevassouras.SCP.Controller;
+
+import br.edu.yuniversidadedevassouras.SCP.Repository.PresencaDAO;
+import br.edu.yuniversidadedevassouras.SCP.model.Presenca;
+import br.edu.yuniversidadedevassouras.SCP.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/presenca")
+public class PresencaController {
+
+    @Autowired
+    private PresencaDAO presencaDAO;
+
+    @GetMapping
+    public Iterable<Presenca> getAll(){
+        return presencaDAO.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public @ResponseBody Optional<Presenca> getPresenca(@PathVariable("id")Long id){
+        return presencaDAO.findById(id);
+    }
+
+    @PostMapping
+    public Presenca postPresenca(@RequestBody Presenca p){
+        return  presencaDAO.save(p);
+    }
+
+    @DeleteMapping
+    public void Delete(){
+        presencaDAO.deleteAll();
+    }
+
+    @PutMapping
+    public Presenca putPresenca(@RequestBody Presenca p){
+        return  presencaDAO.save(p);
+    }
+}
+
---
+++ src/main/java/br/edu/yuniversidadedevassouras/SCP/Repository/PresencaDAO.java
@@ -0,0 +1,7 @@
+package br.edu.yuniversidadedevassouras.SCP.Repository;
+
+import br.edu.yuniversidadedevassouras.SCP.model.Presenca;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PresencaDAO extends CrudRepository<Presenca, Long> {
+}
